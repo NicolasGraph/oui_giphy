c1026cdaf4f3fe14cb5d9033653d5d03326de094
Nicolas Morand <contact@nicolasmorand.com>
2018-08-20 14:41:33 +0200

    Updates the Provider class namespace

--- oui_giphy.php
+++ oui_giphy.php
@@ -31,9 +31,9 @@
 
 namespace Oui;
 
-if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Player\Provider')) {
 
-    class Giphy extends Provider
+    class Giphy extends Player\Provider
     {
         protected static $srcBase = '//giphy.com/';
         protected static $srcGlue = array('embed/', '', '');
